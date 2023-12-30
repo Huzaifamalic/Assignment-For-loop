ef8b81f24b2ef29bae7940953a8036adcae81f7d
Huzaifa Malick <135388170+Huzaifamalic@users.noreply.github.com>
2023-12-30 20:10:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+//                  LOOP Assignment
+// Q1
+// var i;
+// for(i=0;i<=4;i++){
+//     document.write(`<br>Hello world`)
+// }
+//Q2
+// var i;
+// for(i=1;i<=10;i++){
+//     document.write(`${i} <br>`)
+//}
+
+
+//Q3
+// var table = +prompt("Enter your table")
+// var tableendnumber = +prompt("Enter your table number end")
+
+// var i;
+// for(i=1;i<=tableendnumber;i++){
+//     document.write(`${table}x${i}=${i*table}<br>`)
+// }
+//Q4
+
+//  var arr =["nokia","sumsang","apple","motrola"];
+
+//  var i;
+//  for(i=0;i<arr.length;i++){
+//      document.write(arr[i]+"<br>");
+//  }
+
+
+//Q5
+
+// var fruits =["Banana","Mango","apple","greeps"];
+
+// var i;
+// for(i=0;i<fruits.length;i++){
+//     document.write(fruits[i]+"<br>");
+// }
+
+//Q7
+//      conting
+// var i;
+// for(i=1;i<=15;i++){
+//     document.write(i)
+// }
+
+//      Reverce conting
+// var i;
+// for(i=15;i>=1;i--){
+//     document.write(i)
+// }
+
+//      Even
+
+// var i;
+// for(i=0;i<=20;i+=2){
+//     document.write(i)
+// }
+
+// Odd
+// var i;
+// for(i=1;i<=19;i+=2){
+//     document.write(i)
+// }
